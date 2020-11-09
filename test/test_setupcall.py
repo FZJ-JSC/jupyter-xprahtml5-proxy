5e52aea9462c1d82064dc436e95ed150008ad7e1
Jens Henrik Göbbert <j.goebbert@fz-juelich.de>
2020-11-10 00:27:59 +0100

    fix searchpath

--- test/test_setupcall.py
+++ test/test_setupcall.py
@@ -2,7 +2,7 @@ def test_setupcall():
     """
     Test the call of the setup function
     """
-    from .. import jupyter_xprahtml5 as jx
+    import jupyter_xprahtml5 as jx
 
     print("\nRunning test_setupcall...")
     print(jx.setup_html5())
