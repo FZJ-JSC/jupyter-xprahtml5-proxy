540e6086081bccc005d6a072c396729c609aa724
Jens Henrik Göbbert <j.goebbert@fz-juelich.de>
2020-11-10 00:15:41 +0100

    add test

---
+++ test/test_setupcall.py
@@ -0,0 +1,8 @@
+def test_setupcall():
+    """
+    Test the call of the setup function
+    """
+    import jupyter_xprahtml5 as jx
+
+    print("\nRunning test_setupcall...")
+    print(jx.setup_html5())
