1835dcc1413d53ed247f27503be39e5cd1e09bf4
Jens Henrik Göbbert <j.goebbert@fz-juelich.de>
2020-11-10 00:24:31 +0100

    fix test

--- test/test_setupcall.py
+++ test/test_setupcall.py
@@ -2,7 +2,7 @@ def test_setupcall():
     """
     Test the call of the setup function
     """
-    import jupyter_xprahtml5 as jx
+    from .. import jupyter_xprahtml5 as jx
 
     print("\nRunning test_setupcall...")
     print(jx.setup_html5())
