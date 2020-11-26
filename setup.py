2477e406b28d877a657fba93d0c9af4c5a03aa75
Jens Henrik Göbbert <j.goebbert@fz-juelich.de>
2020-11-26 09:54:43 +0100

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ HERE = path.abspath(path.dirname(__file__))
 with open(path.join(HERE, 'README.md'), 'r', encoding = 'utf-8') as fh:
     long_description = fh.read()
 
-version='0.2.3'
+version='0.2.5_devel'
 setup(
     name = 'jupyter-xprahtml5-proxy',
     version = version,
